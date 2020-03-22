ee5c9a80d1ce1c6a46f84f20799256d957e3b2bd
Jovani Tarnowski <jovanitarnowski@gmail.com>
2020-03-22 15:41:58 -0300

    Updates

---
+++ precache-manifest.bb3c9ddee036051bd1533648ab864a45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c757ade504c74fcf7b71e4053863376",
+    "url": "/advice-app-react/index.html"
+  },
+  {
+    "revision": "88cd0806fbd2bc64fd07",
+    "url": "/advice-app-react/static/css/main.24ab8f35.chunk.css"
+  },
+  {
+    "revision": "9b356ef8ea134de648fe",
+    "url": "/advice-app-react/static/js/2.2123628f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advice-app-react/static/js/2.2123628f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88cd0806fbd2bc64fd07",
+    "url": "/advice-app-react/static/js/main.d40a7334.chunk.js"
+  },
+  {
+    "revision": "237d214a644b7090d599",
+    "url": "/advice-app-react/static/js/runtime-main.f5317234.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/advice-app-react/static/media/city.91c109fc.jpg"
+  }
+]);
